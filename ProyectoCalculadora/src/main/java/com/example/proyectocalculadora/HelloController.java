1eb5fd8b12710b3c2d00070b4b4528ea14a07dd1
renatombomio <jesus.mbomio@cesjuanpablosegundo.es>
2021-11-22 19:54:56 +0100

    update

--- ProyectoCalculadora/src/main/java/com/example/proyectocalculadora/HelloController.java
+++ ProyectoCalculadora/src/main/java/com/example/proyectocalculadora/HelloController.java
@@ -1,14 +1,9 @@
 package com.example.proyectocalculadora;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    
 }
---
+++ ProyectoCalculadora/src/main/java/com/example/proyectocalculadora/calculadoraController.java
@@ -0,0 +1,157 @@
+package com.example.proyectocalculadora;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class calculadoraController {
+
+    @FXML
+    private Button btn_borrar;
+
+    @FXML
+    private Button btn_cero;
+
+    @FXML
+    private Button btn_cinco;
+
+    @FXML
+    private Button btn_cuatro;
+
+    @FXML
+    private Button btn_divi;
+
+    @FXML
+    private Button btn_dos;
+
+    @FXML
+    private Button btn_igual;
+
+    @FXML
+    private Button btn_multi;
+
+    @FXML
+    private Button btn_nueve;
+
+    @FXML
+    private Button btn_ocho;
+
+    @FXML
+    private Button btn_porcen;
+
+    @FXML
+    private Button btn_punto;
+
+    @FXML
+    private Button btn_resta;
+
+    @FXML
+    private Button btn_seis;
+
+    @FXML
+    private Button btn_siete;
+
+    @FXML
+    private Button btn_suma;
+
+    @FXML
+    private Button btn_tres;
+
+    @FXML
+    private Button btn_uno;
+
+    @FXML
+    private GridPane labelPantalla;
+
+    @FXML
+    void click_borrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_cero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_cinco(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_cuatro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_divi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_dos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_igual(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_multi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_nueve(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_ocho(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_porcen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_punto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_resta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_seis(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_siete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_suma(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_tres(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_uno(ActionEvent event) {
+
+    }
+
+}
