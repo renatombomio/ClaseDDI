14c958de4ccbb28ed8b1db3e632cc84b1f6d59cd
renatombomio <jesus.mbomio@cesjuanpablosegundo.es>
2022-02-08 10:21:58 +0100

    update

---
+++ Proyecto_ts/index.js
@@ -0,0 +1,4 @@
+var numero = 7;
+var palabra = "Renato";
+var array = [1,2,3,4,5,6,7,8,9];
+console.log("Ejemplo de proyecto ts")
---
+++ Proyecto_ts/index.ts
@@ -0,0 +1,3 @@
+let numero: number =7;
+let palabra: string ="Renato";
+let arrays: [] =[1,2,3,4,5,6,7,8,9];
